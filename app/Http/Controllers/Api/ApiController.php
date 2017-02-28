c6b012319682cd3e479c059ac7c977a140227f46
Nikita Koshkin <nikita.kem@gmail.com>
2017-02-28 16:10:17 +0700

    initial commit

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: nikita.kem
+ * Date: 2/28/17
+ * Time: 4:05 PM
+ */
+class ApiController extends Controller
+{
+
+}
